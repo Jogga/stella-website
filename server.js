59fca6bd88a258941ca64b09ce5e3479dc2757f8
Jogga <joachim@froestl.com>
2016-05-16 11:39:09 -0700

    add(db): enable secure database connection

--- server.js
+++ server.js
@@ -1,17 +1,49 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const config = require('./config.js');
 const app = express();
+var db;
 
 app.use(bodyParser.urlencoded({extended: true}));
+
+// CRUD
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/signup', (req, res) => {
-  console.log(req.body);
-})
-
-app.listen(8000, () => {
-  console.log('listening on 8000');
+  db.collection('emails').save(req.body, (err, result) => {
+    if(err) return console.log(err);
+    console.log('saved to database');
+    res.send('thank you for signing up!');
+  });
 });
+
+// Start Server
+
+MongoClient.connect(
+  mongoUrlHelper(), (err, database) => {
+  if(err) return console.log(err);
+  db = database;
+  app.listen(8000, () => {
+    console.log('listening on 8000');
+  });
+});
+
+// Helper
+
+function mongoUrlHelper() {
+  url = [
+    'mongodb://', 
+    config.mongodb.username, 
+    ':', 
+    config.mongodb.password, 
+    '@',
+    config.mongodb.url, 
+    config.mongodb.database
+  ].join('');
+  return url;
+}
+
